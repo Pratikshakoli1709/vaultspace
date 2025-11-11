67148b24e996ac598d5ad3bd1642c10a1a1f2c14
Pratiksha Koli <pratikshakoli170904@gmail.com>
2025-11-11 04:39:03 +0000

    404 This page could not be found.   not redirecting to dashboard

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -9,7 +9,7 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
+        get: async (name: string) => {
           return cookieStore.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
